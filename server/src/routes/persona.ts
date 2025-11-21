2da78c9f213e49e7fc7c38e51b9519c60b673294
Sunil Panchani <git@techforbs.com>
2025-11-21 16:16:09 +0530

    Create persona.ts

---
+++ server/src/routes/persona.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { getSnapshot } from '../data/snapshots.js'
+
+const personaRouter = Router()
+
+personaRouter.get('/list', (req, res) => {
+  const snapshot = getSnapshot(req.query.partner as string | undefined)
+  res.json(snapshot.personas)
+})
+
+export default personaRouter
+
+
+
+
+
