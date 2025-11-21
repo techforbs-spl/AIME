785b5ffcc8b3a21ae3467d24c577eacd45a6d7ea
Sunil Panchani <git@techforbs.com>
2025-11-21 16:25:20 +0530

    Update postcss.config.js

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,9 @@
+import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/postcss'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
 }
